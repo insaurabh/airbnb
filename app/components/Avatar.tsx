095b32cec6d3f0ff653bf932c4ac89ca6017dafb
Saurabh Ranjan <inrsaurabh@gmail.com>
2023-05-13 23:22:02 +0530

    Static path added for image to check the blured effect

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import Image from 'next/image';
+import avatar from '../../public/images/placeholder/avatar.jpg';
 
 const Avatar = () => {
     return (
         <Image
             className="rounded-full"
+            alt="Avatar"
             height='30'
             width="30"
-            alt="Avatar"
-            src="/images/placeholder/avatar.jpg"
+            src={avatar}
+            priority={true}
+            placeholder="blur"
         />
     );
 }
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import logo from '../../../public/images/logo.png';
 
 const Logo = () => {
     const router = useRouter();
@@ -11,8 +12,9 @@ const Logo = () => {
             className="md-block cursor-pointer"
             height="100"
             width="100"
-            src="/images/logo.png"
+            src={logo}
             priority={true}
+            placeholder="blur"
         />
     )
 }
